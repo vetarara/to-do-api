7e40b7b9e92b39ea03b7b52e9944d8c423f10196
Veta <vetarara@yandex.ru>
2026-07-27 00:08:51 +0300

    add JSX and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
+import './styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-// создаёт корень реакт-приложения - новая схема из реакт 18
 createRoot(document.getElementById('root')).render(
-  // StrictMode - специальная обёртка, которая помогает найти потенциальные проблемы в коде. может дёргать компоненты дважды
   <StrictMode>
     <App />
   </StrictMode>,
